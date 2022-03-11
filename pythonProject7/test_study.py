c185274c0bb721c1001425767b48a87eb924e2e8
kwak-dong-su <srs3576@gmail.com>
2022-03-11 20:19:24 +0900

    python project

--- pythonProject7/test_study.py
+++ pythonProject7/test_study.py
@@ -1,7 +1,23 @@
-list1=['1','2','3','3']
-list2=set(list1)
-list2.add(4)
-list2.add(4)
-list2.add(5)
-print(list(list2))
+
+list1=[1, 3, 2, 5, 7, 10, 6]
+list1.sort(reverse=True)
+list1.reverse()
+print(list1)
+list1.extend(3)
+print(list1)
+
+list1.append(tuple([3, 4, 5]))
+print(list1)
+print(list1[7])
+list1[7]=1
+print(list1)
+
+tuple1=(3, 4, 5, [1, 1, 1, 2])
+print(tuple1)
+tuple1[3][0]=66
+print(tuple1[3])
+print(tuple1)
+
+list2=['a', 'b', 'A', 'B', '1', '2', '가', '나']
+list2.sort()
 print(list2)
